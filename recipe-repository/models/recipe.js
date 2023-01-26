20d55d38cff0d31cec48b8a60ff72cb2298602f1
Gautham Battineni <71089271+gauthambr67@users.noreply.github.com>
2023-01-26 11:35:03 -0500

    adding gitignore

---
+++ recipe-repository/models/recipe.js
@@ -0,0 +1,61 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const recipeSchema = new mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    course: {
+      type: String,
+      required: true,
+      enum: ["Appetizer", "Main Course", "Dessert"],
+    },
+    serves: {
+      type: Number,
+      default: 1,
+      min: 1,
+    },
+    cookTime: {
+      type: Number,
+      min: 1,
+    },
+    prepInst: {
+      type: String,
+      required: true,
+    },
+    cookInst: {
+      type: String,
+      required: true,
+    },
+    ingredients: {
+      type: Schema.Types.ObjectId,
+      ref: "Ingredient",
+      required: true,
+      measure: {
+        type: String,
+        enum: ["ml", "grams", "tablespoon"],
+        required: true,
+      },
+      quantity: {
+        type: Number,
+        required: true,
+      },
+    },
+
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+      required: true,
+    },
+    userName: String,
+    userAvatar: String,
+  },
+  {
+    timestamps: true,
+  }
+);
+
+module.exports = mongoose.model("Recipe", recipeSchema);
