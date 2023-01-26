9549b7aa08f662769599147e51f44e80e600e25a
Gautham Battineni <71089271+gauthambr67@users.noreply.github.com>
2023-01-26 09:59:44 -0500

    adding user model for Googel OAuth2

--- recipe-repository/routes/home.js
+++ recipe-repository/routes/home.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get("/", function (req, res, next) {
-  res.redirect("index", { title: "Recipes" });
+  res.render("home", { title: "Recipe Repository" });
 });
 
 module.exports = router;
