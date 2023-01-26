8ff3914e45bcc171d6dd47cd54eb55f1d6a70dd9
Gautham Battineni <71089271+gauthambr67@users.noreply.github.com>
2023-01-26 11:39:10 -0500

    adding ingredient model

--- recipe-repository/models/ingredient.js
+++ recipe-repository/models/ingredient.js
@@ -1,1 +1,12 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const ingredientSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+});
+
 module.exports = mongoose.model("Ingredient", ingredientSchema);
